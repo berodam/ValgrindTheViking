269dbb3a10414b2ec864a65dc6c57d12886d07ee
berodam <berodam@gmail.com>
2015-06-02 16:45:08 -0400

    First Commit
    all garbage

---
+++ Source/Valgrind/ValgrindGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ValgrindGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALGRIND_API AValgrindGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
